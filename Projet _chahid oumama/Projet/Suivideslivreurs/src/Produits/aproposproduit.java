13d03887450d753be78426a5afd0c9aa5138d4ea
oumamach <118837165+oumamach@users.noreply.github.com>
2023-04-03 21:46:14 +0200

    Add files via upload

---
+++ Projet _chahid oumama/Projet/Suivideslivreurs/src/Produits/aproposproduit.java
@@ -0,0 +1,10 @@
+
+package Produits;
+
+public interface IMalzemeIslemleri {
+    
+    
+    public void malzemeEkle(String isim, String barkod, String tur, String miktar, String fiyat, String kayittarihi);
+    
+    
+}
